6afaed33f5a727fc1ad45c95e4112bcbf1bd21b5
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-03 15:07:35 +0300

    add click event to button action

--- src/components/button.js
+++ src/components/button.js
@@ -4,7 +4,7 @@ class Button extends Component{
     render(){
         return (
             <div>
-                <button class="calc-button">{this.props.symbol}</button>
+                <button class="calc-button" onClick={() => this.props.action(this.props.symbol)}>{this.props.symbol}</button>
             </div>
         )
     }
