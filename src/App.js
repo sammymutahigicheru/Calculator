6582a4f3e907116aa5b60f3b3e228992c49fb1a2
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-03 14:52:00 +0300

    add  all button sysmbols that are needed for the calculator

--- src/App.js
+++ src/App.js
@@ -12,9 +12,28 @@ class App extends Component {
   reset = () =>{
     this.setState({result: '0'});
   }
+  addToCurrent = (symbol) =>{
+    this.setState({current: this.state.current + symbol});
+  }
   render(){
     const buttons = [
-      {symbol: 'C',cols:1,action: this.reset}
+      {symbol: 'C',cols:3,action: this.reset},
+      {symbol:'/',cols:1,action: this.addToCurrent},
+      {symbol:'7',cols:1,action: this.addToCurrent},
+      {symbol:'8',cols:1,action: this.addToCurrent},
+      {symbol:'9',cols:1,action: this.addToCurrent},
+      {symbol:'x',cols:1,action: this.addToCurrent},
+      {symbol:'4',cols:1,action: this.addToCurrent},
+      {symbol:'5',cols:1,action: this.addToCurrent},
+      {symbol:'6',cols:1,action: this.addToCurrent},
+      {symbol:'-',cols:1,action: this.addToCurrent},
+      {symbol:'1',cols:1,action: this.addToCurrent},
+      {symbol:'2',cols:1,action: this.addToCurrent},
+      {symbol:'3',cols:1,action: this.addToCurrent},
+      {symbol:'+',cols:1,action: this.addToCurrent},
+      {symbol:'0',cols:1,action: this.addToCurrent},
+      {symbol:'-',cols:1,action: this.addToCurrent},
+      {symbol:'=',cols:1,action: this.addToCurrent},
     ];
     return (
       <div className="App">
