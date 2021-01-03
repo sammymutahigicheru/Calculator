dd77675b4acdce6de40439ec5f0105f1545095e7
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-03 14:44:27 +0300

    modify app component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React, {Component} from "react";
+import Button from './components/button'
 
 class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      current: "",
+      current: '0',
       previous: []
     }
   }
+  reset = () =>{
+    this.setState({result: '0'});
+  }
   render(){
+    const buttons = [
+      {symbol: 'C',cols:1,action: this.reset}
+    ];
     return (
       <div className="App">
         <input className = "result" type="test" value={this.state.current} />
+
       </div>
     );
   }
